bd270d21fcb0b5067988eb867f76c956c9a2a1df
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-15 13:12:53 -0400

    Test image bundling in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./style.css";
+import Icon from "./dkc.webp";
+
+const element = document.createElement("div");
+
+const myIcon = new Image();
+myIcon.src = Icon;
+
+element.appendChild(myIcon);
+
+document.body.appendChild(element);
