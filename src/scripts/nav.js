b1e44522a2ab5c199b3e3cc7b5ee91cc3fb1f87d
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-15 18:28:21 -0400

    Write function for switching content
    First, a function for removing all of the children of the content div,
    and then uses a module to get a generated div to append to the content
    div.

--- src/scripts/nav.js
+++ src/scripts/nav.js
@@ -1,4 +1,21 @@
+import HomeTab from "./homeTab";
+
 export default function navController() {
   const navTabs = document.querySelectorAll(".nav .tab");
   const contentDiv = document.querySelector(".content");
+  let generatedDiv;
+
+  function removeContent() {
+    while (contentDiv.firstChild) {
+      contentDiv.removeChild(contentDiv.firstChild);
+    }
+  }
+
+  function switchTab(tab) {
+    if (tab === "home") {
+      generatedDiv = HomeTab();
+      removeContent();
+      contentDiv.appendChild(generatedDiv);
+    }
+  }
 }
