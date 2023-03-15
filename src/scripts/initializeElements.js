dee2a76087118b9cee4914d79551b6765dce3e12
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-15 16:52:50 -0400

    Add param for setting text content in createElement()

--- src/scripts/initializeElements.js
+++ src/scripts/initializeElements.js
@@ -1,4 +1,4 @@
-function createElement(elementType, classes, parent) {
+function createElement(elementType, classes, parent, textContent) {
   const element = document.createElement(elementType);
 
   if (classes) {
@@ -11,6 +11,10 @@ function createElement(elementType, classes, parent) {
     parent.appendChild(element);
   }
 
+  if (textContent) {
+    element.textContent = textContent;
+  }
+
   return element;
 }
 
@@ -20,17 +24,22 @@ export default function createInitialElements() {
 
   // Create Header Elements w/ Classes
   const headerDiv = createElement("div", ["header"], containerDiv);
-  const headerTitle = createElement("p", ["title"], headerDiv);
+  const headerTitle = createElement(
+    "p",
+    ["title"],
+    headerDiv,
+    "The Legerdemain Lounge"
+  );
 
   // Create Nav Elements w/ Classes
   const navDiv = createElement("div", ["nav"], containerDiv);
   const navTabTray = createElement("div", ["tab-tray"], navDiv);
   const homeTab = createElement("div", ["tab", "home"], navTabTray);
-  const homeTabText = createElement("p", [], homeTab);
+  const homeTabText = createElement("p", [], homeTab, "Home");
   const menuTab = createElement("div", ["tab", "menu"], navTabTray);
-  const menuTabText = createElement("p", [], menuTab);
+  const menuTabText = createElement("p", [], menuTab, "Menu");
   const contactTab = createElement("div", ["tab", "contact"], navTabTray);
-  const contactTabText = createElement("p", [], contactTab);
+  const contactTabText = createElement("p", [], contactTab, "Contact");
 
   // Create Content Elements
 
