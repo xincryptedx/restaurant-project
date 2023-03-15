1ea1f0172fae37d0e84f0821feb5a00024384166
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-15 17:21:22 -0400

    Create main children divs for home tab content

--- src/scripts/homeTab.js
+++ src/scripts/homeTab.js
@@ -3,5 +3,11 @@ import { createElement } from "./initializeElements";
 export default function createHomeTab() {
   const element = createElement("div", ["home"]);
 
+  // Create Children Elements
+  createElement("div", ["about"], element);
+  createElement("div", ["hours"], element);
+  createElement("div", ["locations"], element);
+  createElement("div", ["reviews"], element);
+
   return element;
 }
