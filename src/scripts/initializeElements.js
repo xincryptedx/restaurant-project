36dc81975e250f2c0fab26c9ce64ddf9a48490a9
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-15 16:42:32 -0400

    Write createElement function
    This function will take a required element type as a string, an optional
    array of strings that will be added as classes, and an optional parent
    to append to.
    
    Also replaced all .nav element creation with this new function.

--- src/scripts/initializeElements.js
+++ src/scripts/initializeElements.js
@@ -1,3 +1,19 @@
+function createElement(elementType, classes, parent) {
+  const element = document.createElement(elementType);
+
+  if (classes) {
+    classes.forEach((className) => {
+      element.classList.add(className);
+    });
+  }
+
+  if (parent) {
+    parent.appendChild(element);
+  }
+
+  return element;
+}
+
 export default function createInitialElements() {
   // Create Container Elements
   const containerDiv = document.createElement("div");
@@ -12,34 +28,22 @@ export default function createInitialElements() {
   headerDiv.appendChild(headerTitle);
 
   // Create Nav Elements w/ Classes
-  const navDiv = document.createElement("div");
-  navDiv.classList.add("nav");
-  const navTabTray = document.createElement("div");
-  navTabTray.classList.add("tab-tray");
-  const homeTab = document.createElement("div");
-  homeTab.classList.add("tab", "home");
-  const menuTab = document.createElement("div");
-  menuTab.classList.add("tab", "menu");
-  const contactTab = document.createElement("div");
-  contactTab.classList.add("tab", "contact");
-  const homeTabText = document.createElement("p");
-  homeTabText.textContent = "Home";
-  const menuTabText = document.createElement("p");
-  menuTabText.textContent = "Menu";
-  const contactTabText = document.createElement("p");
-  contactTabText.textContent = "Contact";
-  // Append Nav Children
-  navDiv.appendChild(navTabTray);
-  navTabTray.appendChild(homeTab);
-  navTabTray.appendChild(menuTab);
-  navTabTray.appendChild(contactTab);
-  homeTab.appendChild(homeTabText);
-  menuTab.appendChild(menuTabText);
-  contactTab.appendChild(contactTabText);
+  const navDiv = createElement("div", ["nav"], containerDiv);
+  const navTabTray = createElement("div", ["tab-tray"], navDiv);
+  const homeTab = createElement("div", ["tab", "home"], navTabTray);
+  const homeTabText = createElement("p", [], homeTab);
+  const menuTab = createElement("div", ["tab", "menu"], navTabTray);
+  const menuTabText = createElement("p", [], menuTab);
+  const contactTab = createElement("div", ["tab", "contact"], navTabTray);
+  const contactTabText = createElement("p", [], contactTab);
+
+  // Create Content Elements
+
+  // Create Footer Elements
+  createElement("div", ["footer"], containerDiv);
 
   // Append Container Children
   containerDiv.appendChild(headerDiv);
-  containerDiv.appendChild(navDiv);
 
   // Append to Body
   document.body.appendChild(containerDiv);
