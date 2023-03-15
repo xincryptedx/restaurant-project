e6e0de26c4485cf626bcdf4064bb624db34937d3
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-15 13:35:04 -0400

    Configure webpack for development and use inline source map

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require("path"); //node.js utility
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
+  devtool: "inline-source-map",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
