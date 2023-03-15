336845fe2db8e07c7dc03b409ec5a51bc35acb84
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-15 17:31:50 -0400

    Add reference to .content for appending generated div later

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import InitializeElements from "./scripts/initializeElements";
 import HomeTab from "./scripts/homeTab";
 
 InitializeElements();
+
+const contentDiv = document.querySelector(".content");
+
+const generatedDiv = HomeTab();
+contentDiv.appendChild(generatedDiv);
