de7908a903e5c34f30d25bb119eb9807a2cd13d5
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-15 12:50:34 -0400

    Configure basic webpack export settings

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require("path"); //node.js utility
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
