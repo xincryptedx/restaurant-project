b5962652d77ce07ddf4f9e642b68af431e714563
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-15 15:41:28 -0400

    Remove test font and image

--- src/index.js
+++ src/index.js
@@ -1,15 +1,4 @@
 import "./style.css";
 import InitializeElements from "./scripts/initializeElements";
-import Icon from "./dkc.webp";
 
-const element = document.createElement("div");
-const someText = document.createElement("p");
-
-const myIcon = new Image();
-myIcon.src = Icon;
-someText.textContent = "Arthas, my son... No king rules forever.";
-
-element.appendChild(myIcon);
-element.appendChild(someText);
-
-document.body.appendChild(element);
+console.log("hello");
