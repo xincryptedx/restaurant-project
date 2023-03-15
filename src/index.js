5017b352914b53cd23fdcd3ca40387943b2ee79e
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-15 13:26:45 -0400

    Configure webpack for fonts and test it

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import "./style.css";
 import Icon from "./dkc.webp";
 
 const element = document.createElement("div");
+const someText = document.createElement("p");
 
 const myIcon = new Image();
 myIcon.src = Icon;
+someText.textContent = "Arthas, my son...";
 
 element.appendChild(myIcon);
+element.appendChild(someText);
 
 document.body.appendChild(element);
