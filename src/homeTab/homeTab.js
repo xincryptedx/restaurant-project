dff590d33ff9e2229561a6ee5c78ac542b06bca4
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-16 17:43:19 -0400

    Create json file with text for home div

--- src/homeTab/homeTab.js
+++ src/homeTab/homeTab.js
@@ -3,7 +3,6 @@ import { createElement } from "../scripts/initializeElements";
 export default function createHomeDiv() {
   const element = createElement("div", ["home"]);
 
-  // Create Children Elements
   createElement("div", ["about"], element);
   createElement("div", ["hours"], element);
   createElement("div", ["locations"], element);
