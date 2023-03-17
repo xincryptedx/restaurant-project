27f02e1d3ba09d45ab0ec141fcacb39ac19d367d
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-17 19:50:55 -0400

    Add some text content for menu and create some child divs

--- src/menuTab/menuTab.js
+++ src/menuTab/menuTab.js
@@ -1,21 +1,55 @@
 import { createElement } from "../scripts/initializeElements";
+import TextContent from "./textContent.json";
 
 export default function createMenuDiv() {
   const element = createElement("div", ["menu"]);
 
-  createElement("div", ["section", "title", "appetizers"], element);
-  createElement("div", ["wrapper", "appetizers"], element);
-  // Create items based on json
+  createElement(
+    "div",
+    ["section", "title", "appetizers"],
+    element,
+    TextContent.appetizers.title
+  );
+  const appetizersWrapper = createElement(
+    "div",
+    ["wrapper", "appetizers"],
+    element
+  );
+  Object.keys(TextContent.appetizers).forEach((key) => {
+    if (key !== "title") {
+      createElement(
+        "div",
+        ["entry", "appetizers"],
+        appetizersWrapper,
+        TextContent.appetizers[key].value
+      );
+    }
+  });
 
-  createElement("div", ["section", "title", "entrees"], element);
+  createElement(
+    "div",
+    ["section", "title", "entrees"],
+    element,
+    TextContent.entrees.title
+  );
   createElement("div", ["wrapper", "entrees"], element);
   // Create items based on json
 
-  createElement("div", ["section", "title", "beverages"], element);
+  createElement(
+    "div",
+    ["section", "title", "beverages"],
+    element,
+    TextContent.beverages.title
+  );
   createElement("div", ["wrapper", "beverages"], element);
   // Create items based on json
 
-  createElement("div", ["section", "title", "desserts"], element);
+  createElement(
+    "div",
+    ["section", "title", "desserts"],
+    element,
+    TextContent.desserts.title
+  );
   createElement("div", ["wrapper", "desserts"], element);
   // Create items based on json
 
