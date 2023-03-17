b257b802c836368561d061ad444a8b166ff1fef5
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-17 18:12:39 -0400

    Create main menu child divs

--- src/menuTab/menuTab.js
+++ src/menuTab/menuTab.js
@@ -3,5 +3,21 @@ import { createElement } from "../scripts/initializeElements";
 export default function createMenuDiv() {
   const element = createElement("div", ["menu"]);
 
+  createElement("div", ["section", "title", "appetizers"], element);
+  createElement("div", ["wrapper", "appetizers"], element);
+  // Create items based on json
+
+  createElement("div", ["section", "title", "entrees"], element);
+  createElement("div", ["wrapper", "entrees"], element);
+  // Create items based on json
+
+  createElement("div", ["section", "title", "beverages"], element);
+  createElement("div", ["wrapper", "beverages"], element);
+  // Create items based on json
+
+  createElement("div", ["section", "title", "desserts"], element);
+  createElement("div", ["wrapper", "desserts"], element);
+  // Create items based on json
+
   return element;
 }
