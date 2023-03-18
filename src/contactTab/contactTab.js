01f273955a16446c6353799681d4a8dbe379c0af
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-18 14:25:18 -0400

    Import background image

--- src/contactTab/contactTab.js
+++ src/contactTab/contactTab.js
@@ -1,4 +1,5 @@
 import { createElement } from "../scripts/initializeElements";
+import BackgroundImage from "../content/squareParchment.svg";
 
 export default function createContactDiv() {
   const element = createElement("div", ["contact"]);
