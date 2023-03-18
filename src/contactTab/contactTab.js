5f7bc5a115553ea636a7276f31e8ccad160bbd5e
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-18 14:27:39 -0400

    Create elements for leylines div in contact

--- src/contactTab/contactTab.js
+++ src/contactTab/contactTab.js
@@ -1,8 +1,20 @@
 import { createElement } from "../scripts/initializeElements";
 import BackgroundImage from "../content/squareParchment.svg";
+import TextContent from "./textContent.json";
 
 export default function createContactDiv() {
   const element = createElement("div", ["contact"]);
+
+  const leylinesDiv = createElement("div", ["leylines"], element);
+  createElement("p", ["title"], leylinesDiv, TextContent.leylines.title); // Don't try to join single lined text
+  createElement(
+    "p",
+    ["text"],
+    leylinesDiv,
+    TextContent.leylines.text.join(" ")
+  ); // Multiline text needs joined
+  const aboutBgImg = createElement("img", ["background"], leylinesDiv);
+  aboutBgImg.src = BackgroundImage;
 
   return element;
 }
